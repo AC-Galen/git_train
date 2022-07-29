09f5c94751ff4db7787c88c508a45f3d69926d2b
AC-Galen <lottaie7359882@gmail.com>
2022-07-29 11:08:30 +0800

    feat: add model & routes

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+const routes = require('./routes')
 
 app.get('/', (req,res) => {
   res.send('Hello World')
 })
+
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`)
