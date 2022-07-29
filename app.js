9b00202d56c4192f76d868be5920a78fe2b4db07
AC-Galen <lottaie7359882@gmail.com>
2022-07-29 10:52:27 +0800

    feat: add views/layouts folder & install handlebars

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 
 
-app.get('/', (req,res) => {
-  res.send('Hello World')
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
 app.listen(PORT, () => {
